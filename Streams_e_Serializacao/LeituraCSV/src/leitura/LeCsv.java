517d3e32d49ee781f1033cb88a1d6630ff74a200
Marcos Antonio Carneiro de Paula <cardepaula@gmail.com>
2019-06-13 13:57:29 -0300

    feat: Exercicio LeituraCSV

--- Streams_e_Serializacao/LeituraCSV/src/leitura/LeCsv.java
+++ Streams_e_Serializacao/LeituraCSV/src/leitura/LeCsv.java
@@ -10,13 +10,29 @@ public class LeCsv {
         
      public LinkedList<Cliente> leCsvClientes() throws IOException{         
       
-         LinkedList<Cliente> clientes = new  LinkedList<Cliente>();             
+        LinkedList<Cliente> clientes = new  LinkedList<Cliente>();             
         String current = new java.io.File( "." ).getCanonicalPath();        
         File inputFile = new File(current + "\\src\\arquivo.csv");                
         FileReader in = new FileReader(inputFile);
         int c;
-        while ((c = in.read()) != -1) {
-            c = in.read();            
+        String input = "";
+        boolean primeiraLinha = false;
+        
+        while(!primeiraLinha){
+            c = in.read();
+            if (c == (int)('\n')){
+                primeiraLinha = true;
+            }
+        }
+        
+        while ((c = in.read()) != -1) { 
+            if ((char)(c) != '\n'){
+                  input += (char)(c);
+            }else{
+                Cliente cliente = preencheCliente(input);
+                clientes.add(cliente);
+                input = "";
+            }
         }       
         in.close(); 
         
@@ -24,5 +40,19 @@ public class LeCsv {
         //rode o teste MainReadCsvTest.java para testar o seu código.
         
         return clientes;
+     }
+     
+     private Cliente preencheCliente(String linha){
+         
+         String dados[] = linha.split(";");
+         Cliente cliente = new Cliente();
+         
+         cliente.setId(dados[0]);
+         cliente.setNome(dados[1]);
+         cliente.setEmail(dados[2]);
+         cliente.setTelefone(dados[4]);
+         cliente.setTotalCompras(Double.parseDouble(dados[5]));
+        
+         return cliente;
      }
 }
